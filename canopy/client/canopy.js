82a79647746359fcdea727daa17d3dbb282c30be
Stephanie Yu <styu314@gmail.com>
2013-11-03 12:31:01 -0500

    Created admin accounts, refactored canopy.html to include login logic

--- canopy/client/canopy.js
+++ canopy/client/canopy.js
@@ -3,16 +3,15 @@ console.log("client-canopy");
 
 if (Meteor.isClient) {
 
-  Template.hello.greeting = function () {
-    if (Meteor.user()){
-      t = Meteor.render(function() {
-      	return Template.letsgo();
-      });
-      // this can't be the right way to do it… :(
-      document.body.appendChild(t);
-      return "HELLO, " + Meteor.user().emails[0].address;
-    } else {
-      return "Curious.. you don't seem to be logged in";
+  Template.login.greeting = function () {
+    if (!Meteor.user()) {
+      return "Curious...you don't seem to be logged in";
     }
   };
+
+  Template.content.greeting = function() {
+    if (Meteor.user()) {
+      return "HELLO, " + Meteor.user().emails[0].address;
+    }
+  }
 }
--- canopy/server/canopy.js
+++ canopy/server/canopy.js
@@ -1,40 +1,118 @@
+;(function () {
 
-/*
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to canopy.";
-  };
+  "use strict";
 
-  Template.hello.events({
-    'click #go' : function (evt) {
-      // template data, if any, is available in 'this'
-      console.log(this);
-      username = $('#username').val();
-      password = $('#password').val();
-      Meteor.loginWithPassword(username, password, function(err){
-        if (err){
-          console.log("HELLO! you fucked up");
-        } else {
-          // The user has been logged in.
-          console.log("HELLO! You bitch be loggin in, arg");
-        }
+
+////////////////////////////////////////////////////////////////////
+// Startup
+//
+
+Meteor.startup(function () {
+
+  ////////////////////////////////////////////////////////////////////
+  // Create Test Secrets
+  //
+    
+  // if (Meteor.secrets.find().fetch().length === 0) {
+  //   Meteor.secrets.insert({secret:"ec2 password: apple2"});
+  //   Meteor.secrets.insert({secret:"domain registration pw: apple3"});
+  // }
+
+
+  ////////////////////////////////////////////////////////////////////
+  // Create Test Users
+  //
+
+  if (Meteor.users.find().fetch().length === 0) {
+
+    console.log('Creating users: ');
+
+     var users = [
+        //{name:"Normal User",email:"normal@example.com",roles:[]},
+        {name:"Steph Yu",email:"styu@mit.edu",roles:['admin']},
+        {name:"Victor H",email:"vhung@mit.edu",roles:['admin']},
+        //{name:"Admin User",email:"admin@example.com",roles:['admin']}
+      ];
+
+    _.each(users, function (userData) {
+      var id,
+          user;
+      
+      console.log(userData);
+
+      var password = Math.random().toString(36).substring(2, 7);
+      id = Accounts.createUser({
+        email: userData.email,
+        password: password,
+        profile: { name: userData.name }
       });
-    }
-  });
-}
-*/
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-   /*
- Accounts.createUser({username: "bob",
-    	email: "bob@bob.bob",
-    	password: "notbob",
-    	profile: {}});
-*/
-  //console.log(Meteor.users);
-  });
+      // email verification
+      Email.send({
+        to: userData.email,
+        from: '6470-staff@mit.edu',
+        replyTo: '6470-staff@mit.edu',
+        subject: 'Your Meteor Admin Account',
+        html: 'Hello ' + userData.name + ',<br /><br />' +
+              'Your JungleAdventures Admin Account has been created. Please login to <url> with your email and the following password: ' + password + '<br /><br />' +
+              'Thanks,<br />' +
+              'Your Friendly 6.470 Monkey'
+      });
+      Meteor.users.update({_id: id}, {$set:{'emails.0.verified': true}});
 
-}
+      Roles.addUsersToRoles(id, userData.roles);
+    
+    });
+  }
 
+
+
+  ////////////////////////////////////////////////////////////////////
+  // Prevent non-authorized users from creating new users
+  //
+
+  // Accounts.validateNewUser(function (user) {
+  //   var loggedInUser = Meteor.user();
+
+  //   if (Roles.userIsInRole(loggedInUser, ['admin','manage-users'])) {
+  //     return true;
+  //   }
+
+  //   throw new Meteor.Error(403, "Not authorized to create new users");
+  // });
+
+});
+
+
+////////////////////////////////////////////////////////////////////
+// Publish
+//
+
+
+// Authorized users can view secrets
+// Meteor.publish("secrets", function () {
+//   var user = Meteor.users.findOne({_id:this.userId});
+
+//   if (Roles.userIsInRole(user, ["admin","view-secrets"])) {
+//     console.log('publishing secrets', this.userId)
+//     return Meteor.secrets.find();
+//   }
+
+//   this.stop();
+//   return;
+// });
+
+// // Authorized users can manage user accounts
+// Meteor.publish("users", function () {
+//   var user = Meteor.users.findOne({_id:this.userId});
+
+//   if (Roles.userIsInRole(user, ["admin","manage-users"])) {
+//     console.log('publishing users', this.userId)
+//     return Meteor.users.find({}, {fields: {emails: 1, profile: 1, roles: 1}});
+//   } 
+
+//   this.stop();
+//   return;
+// });
+
+}());
