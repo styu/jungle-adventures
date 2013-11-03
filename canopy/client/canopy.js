20dd08406be7f30994ad1c9aee8825af881284f6
poofytoo <victorhung92@gmail.com>
2013-11-03 01:58:48 -0500

    New template once logged in..?

--- canopy/client/canopy.js
+++ canopy/client/canopy.js
@@ -1,1 +1,18 @@
 console.log("client-canopy");
+
+
+if (Meteor.isClient) {
+
+  Template.hello.greeting = function () {
+    if (Meteor.user()){
+      t = Meteor.render(function() {
+      	return Template.letsgo();
+      });
+      // this can't be the right way to do it… :(
+      document.body.appendChild(t);
+      return "HELLO, " + Meteor.user().emails[0].address;
+    } else {
+      return "Curious.. you don't seem to be logged in";
+    }
+  };
+}
--- canopy/server/canopy.js
+++ canopy/server/canopy.js
@@ -33,6 +33,8 @@ if (Meteor.isServer) {
     	password: "notbob",
     	profile: {}});
 */
+  //console.log(Meteor.users);
   });
+
 }
 
